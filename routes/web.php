ec92a76a799d0f03f03fb2dace2e6af2f068ebee
Kantemir Satibalov <kantik001@yandex.ru>
2023-12-05 16:42:02 +0300

    first step

---
+++ routes/web.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+
+Route::get('/', \App\Http\Controllers\Main\IndexController::class);
+
+
+Route::prefix('admin')->group(function () {
+        Route::get('/', \App\Http\Controllers\Admin\Main\IndexController::class);
+        Route::prefix('categories')->group(function () {
+            Route::get('/',[ \App\Http\Controllers\Admin\Category\CategoryController::class, 'index']);
+    });
+});
+
+
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
