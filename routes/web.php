8bb48c573a21ecddb0ecde242c5d669c4fa9adb1
Kantemir Satibalov <kantik001@yandex.ru>
2024-01-16 10:20:41 +0300

    добавили middleware and breeze

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,20 +14,18 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-
-Route::get('/', \App\Http\Controllers\Main\IndexController::class);
-
-
-Route::prefix('admin')->group(function () {
-        Route::get('/', \App\Http\Controllers\Admin\Main\IndexController::class)->name('admin.main.index');
-        Route::resource('categories', \App\Http\Controllers\Admin\Category\CategoryController::class);
-        Route::resource('tags', \App\Http\Controllers\Admin\Tag\TagController::class);
-        Route::resource('posts', \App\Http\Controllers\Admin\Post\PostController::class);
-        Route::resource('users', \App\Http\Controllers\Admin\User\UserController::class);
+Route::get('/', function () {
+    return view('welcome');
 });
 
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
 
-Auth::routes();
-
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+require __DIR__.'/auth.php';
